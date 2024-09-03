6b73d4ea6cd4447e09715d97ea6f3ac5d7efffaa
Aungkokolin1997 <lin@quartile.co>
2024-09-03 08:40:08 +0000

    [ADD] portal_view_adjust

---
+++ portal_view_adjust/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2024 Quartile (https://www.quartile.co)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Portal View Adjust",
+    "category": "Portal",
+    "version": "15.0.1.0.0",
+    "author": "Quartile",
+    "website": "https://www.quartile.co",
+    "license": "LGPL-3",
+    "depends": ["portal"],
+    "data": [
+        "views/portal_templates.xml",
+    ],
+    "installable": True,
+}
